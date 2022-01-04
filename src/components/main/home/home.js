0bfc15acab69188d7c55a496e687156c4a0db0b5
Marco Prouve <marcoprouve@gmail.com>
2022-01-03 21:48:07 -0800

    Cleared cache on single searches. added total items to result tabs.

--- src/components/main/home/home.js
+++ src/components/main/home/home.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react"
-import filter from "lodash/filter"
-import { useSelector } from "react-redux"
-import Button from "@mui/material/Button"
-import SearchForm from "./components/search-form/search-form.js"
-import Dragons from "../../partials/dragons/dragons.js"
-import Eggs from "../../partials/eggs/eggs.js"
+import React, { useState } from 'react'
+import filter from 'lodash/filter'
+import { useSelector } from 'react-redux'
+import Button from '@mui/material/Button'
+import SearchForm from './components/search-form/search-form.js'
+import Dragons from '../../partials/dragons/dragons.js'
+import Eggs from '../../partials/eggs/eggs.js'
 import {
   MainContainer,
   BannerOuterContainer,
@@ -17,25 +17,27 @@ import {
   Separator,
   TabsContainer,
   Tab,
-  ContentContainer,
-} from "./styled-components/home.js"
+  ContentContainer
+} from './styled-components/home.js'
 
 const styles = {
   chromeButton: {
-    display: "inline-block",
-    width: "12rem",
-    minHeight: "2.3rem",
-    height: "2.3rem",
-    margin: "0 .5rem",
-    lineHeight: "1rem",
-    verticalAlign: "middle",
-  },
+    display: 'inline-block',
+    width: '12rem',
+    minHeight: '2.3rem',
+    height: '2.3rem',
+    margin: '0 .5rem',
+    lineHeight: '1rem',
+    verticalAlign: 'middle'
+  }
 }
 
 const Home = () => {
-  const [tab, setTab] = useState("dragons")
+  const [tab, setTab] = useState('dragons')
   const dragons = useSelector(({ dragons }) => dragons)
   const eggs = useSelector(({ eggs }) => eggs)
+  const dragonsQuery = useSelector(({ dragonsQuery }) => dragonsQuery)
+  const eggsQuery = useSelector(({ eggsQuery }) => eggsQuery)
   const unhatchedEggs = filter(eggs, (egg) => {
     return !egg.isHatched
   })
@@ -43,12 +45,14 @@ const Home = () => {
     return egg.isHatched
   })
 
+  console.log({ dragonsQuery, eggsQuery })
+
   return (
     <MainContainer>
       <BannerOuterContainer>
         <BannerInnerContainer>
           <BannerText>
-            Have you downloaded{" "}
+            Have you downloaded{' '}
             <a
               href="https://discordapp.com/users/554028295990018050"
               target="_blank"
@@ -68,9 +72,8 @@ const Home = () => {
         <InnerContainer>
           <Heading>Welcome CryptoDragons Owners</Heading>
           <Subheading>
-            Thank you for stopping by. You can use this form to get detailed
-            info on dragons and eggs, including <b>Arena Strength</b>,{" "}
-            <b>Arena Record</b>, <b>Firstborn Status</b>,{" "}
+            Thank you for stopping by. You can use this form to get detailed info on dragons and
+            eggs, including <b>Arena Strength</b>, <b>Arena Record</b>, <b>Firstborn Status</b>,{' '}
             <b>Blacklist Status</b>, <b>Total Trait Score</b>, and more.
           </Subheading>
 
@@ -81,25 +84,35 @@ const Home = () => {
       </HeroContainer>
 
       <TabsContainer>
-        <Tab selected={tab === "dragons"} onClick={() => setTab("dragons")}>
-          {`Dragons (${dragons.length})`} <div />
-        </Tab>
+        <Tab selected={tab === 'dragons'} onClick={() => setTab('dragons')}>
+          {Object.keys(dragonsQuery).length > 0
+            ? `Dragons (${dragons.length}/${dragonsQuery.totalItems})`
+            : `Dragons (${dragons.length})`}
 
-        <Tab selected={tab === "eggs"} onClick={() => setTab("eggs")}>
-          {`Eggs (${unhatchedEggs.length})`}
           <div />
         </Tab>
 
-        <Tab selected={tab === "eggshells"} onClick={() => setTab("eggshells")}>
-          {`Eggshells (${hatchedEggs.length})`}
+        <Tab selected={tab === 'eggs'} onClick={() => setTab('eggs')}>
+          {Object.keys(eggsQuery).length > 0 && !eggsQuery.isHatched
+            ? `Eggs (${unhatchedEggs.length}/${eggsQuery.totalItems})`
+            : `Eggs (${unhatchedEggs.length})`}
+
+          <div />
+        </Tab>
+
+        <Tab selected={tab === 'eggshells'} onClick={() => setTab('eggshells')}>
+          {Object.keys(eggsQuery).length > 0 && eggsQuery.isHatched
+            ? `Eggshells (${hatchedEggs.length}/${eggsQuery.totalItems})`
+            : `Eggshells (${hatchedEggs.length})`}
+
           <div />
         </Tab>
       </TabsContainer>
 
       <ContentContainer>
-        {tab === "dragons" && <Dragons dragons={dragons} />}
-        {tab === "eggs" && <Eggs eggs={unhatchedEggs} isHatched={false} />}
-        {tab === "eggshells" && <Eggs eggs={hatchedEggs} isHatched={true} />}
+        {tab === 'dragons' && <Dragons dragons={dragons} />}
+        {tab === 'eggs' && <Eggs eggs={unhatchedEggs} isHatched={false} />}
+        {tab === 'eggshells' && <Eggs eggs={hatchedEggs} isHatched={true} />}
       </ContentContainer>
     </MainContainer>
   )
