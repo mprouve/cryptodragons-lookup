495e807296a40e20b01936518caf40ad0c8a7de7
Marco Prouve <marcoprouve@gmail.com>
2022-01-03 22:32:45 -0800

    Added google analytics

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import "react-app-polyfill/ie9"
-import React from "react"
-import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
-import { Provider } from "react-redux"
-import { ThemeProvider } from "@mui/material/styles"
-import "./index.css"
-import config from "./config.js"
-import theme from "./res/mui-theme.js"
-import { GlobalStyles } from "./res/global-styles"
-import { store } from "./redux/store.js"
-import App from "./components/App.js"
-import reportWebVitals from "./reportWebVitals"
+import 'react-app-polyfill/ie9'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import ReactGA from 'react-ga'
+import { ThemeProvider } from '@mui/material/styles'
+import './index.css'
+import config from './config.js'
+import theme from './res/mui-theme.js'
+import { GlobalStyles } from './res/global-styles'
+import { store } from './redux/store.js'
+import App from './components/App.js'
+import reportWebVitals from './reportWebVitals'
 
-config.debug &&
-  console.log("[REACT_ENVIRONMENT]: ", process.env.REACT_APP_NODE_ENV)
-config.debug && console.log("[ENVIRONMENT]: ", process.env.NODE_ENV)
-config.debug && console.log("[REDUX STORE]: ", store.getState())
+config.debug && console.log('[REACT_ENVIRONMENT]: ', process.env.REACT_APP_NODE_ENV)
+config.debug && console.log('[ENVIRONMENT]: ', process.env.NODE_ENV)
+config.debug && console.log('[REDUX STORE]: ', store.getState())
+
+ReactGA.initialize('G-2TENCMP8FR')
 
 ReactDOM.render(
   <Provider store={store}>
@@ -27,7 +29,7 @@ ReactDOM.render(
       </ThemeProvider>
     </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
